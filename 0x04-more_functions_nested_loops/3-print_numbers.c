852683a7d9a2a98e1dcc428f0698193f20353d00
root <vincentjayden49@gmail.com>
2021-02-12 12:44:53 +0000

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints 2 to 20.
+ * @c: counter variable.
+ * _putchar: prints list numbers.
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
