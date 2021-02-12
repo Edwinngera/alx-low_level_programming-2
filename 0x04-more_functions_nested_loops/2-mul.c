6760c0562ff78179667ccd1edd753dc6ace66e01
root <vincentjayden49@gmail.com>
2021-02-12 11:42:26 +0000

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - check the code for Holberton School students.
+ * @a: multiplication variable.
+ * @b: multiplication variable.
+ * Return: multiplication of a and b.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
