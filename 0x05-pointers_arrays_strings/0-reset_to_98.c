cbc9353f7001439a8298bca157f5f397dbf7e916
root <vincentjayden49@gmail.com>
2021-02-16 10:52:51 +0000

    initial and reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ *@n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+int s = 98;
+*n = s;
+}
