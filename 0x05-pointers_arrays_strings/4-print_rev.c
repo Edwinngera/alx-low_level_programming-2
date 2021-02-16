97cea9b854a861917653dc62650397d97822dd23
root <vincentjayden49@gmail.com>
2021-02-16 15:45:27 +0000

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
-
+/**
+ * print_rev - reverses a string
+ *@s: - holds the variable for the string
+ * Return: Always 0.
+ */
 void print_rev(char *s)
 {
-  int k;
-  for (k = 0; s[k] != 0; k++)
-    {
+int k;
+for (k = 0; s[k] != 0; k++)
+{
 
 }
-  for (k = k - 1; k >= 0; k--)
-    {
-      _putchar(s[k]);
+for (k = k - 1; k >= 0; k--)
+{
+_putchar(s[k]);
 }
-  _putchar('\n');
+_putchar('\n');
 }
