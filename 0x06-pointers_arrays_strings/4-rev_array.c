ba7f63f0295e42070c2e6a54bdb36296ea777862
root <vincentjayden49@gmail.com>
2021-02-19 17:50:27 +0000

    update reverse numbers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,23 @@
 #include "holberton.h"
-
+/**
+ * reverse_array - function that array numbers
+ * @a: paramater for array
+ * @n: paramater for array count
+ * Return: Always 0.
+ */
 void reverse_array(int *a, int n)
 {
-  int start;
-  start = 0;
+int start;
+start = 0;
 
-  while (start < n)
-    {
-      int temp = a[start];
-      a[start] = a[n];
-      a[n] = temp;
-      start++;
-      n--;
-    }
+while (start < n)
+{
+int temp = a[start];
+a[start] = a[n];
+a[n] = temp;
+start++;
+n--;
+}
 }
 
 
