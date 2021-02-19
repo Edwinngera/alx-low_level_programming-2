15664acbb8d8638d1448a07fc9f3b825189eff63
VinneyJ <39193976+VinneyJ@users.noreply.github.com>
2021-02-19 18:46:25 +0300

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 /**
- * _strncpy - function that copys strings
- * @dest: paramater for string 2
- * @src: paramater for string 1
- * @n: integer
+ * _strncpy - check the code for Holberton School students.
+ * @src: parameter to a src to copy
+ * @dest: parameter for dest
+ * @n: parameter for number of bytes
  * Return: Always 0.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int a = 0;
+	int b = 0;
 
-for (i = 0; i < n && *(src + i) != '\0'; i++)
-{
-*(dest + i) = *(src + i);
-i++;
-}
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+	while (a != n)
+	{
+		dest[b] = src[a];
+		b++;
+		a++;
+		if (src[a] == '\0')
+		{
+			break;
+		}
+	}
+	while (b != n)
+		dest[b++] = '\0';
+	return (dest);
 }
