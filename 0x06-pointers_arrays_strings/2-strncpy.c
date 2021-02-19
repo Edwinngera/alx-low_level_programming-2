13d5f9e96c6772114c63ca3f04bf91896e97c603
VinneyJ <39193976+VinneyJ@users.noreply.github.com>
2021-02-19 18:48:00 +0300

    update strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
+int a = 0;
+int b = 0;
 
-	while (a != n)
-	{
-		dest[b] = src[a];
-		b++;
-		a++;
-		if (src[a] == '\0')
-		{
-			break;
-		}
-	}
-	while (b != n)
-		dest[b++] = '\0';
-	return (dest);
+while (a != n)
+{
+dest[b] = src[a];
+b++;
+a++;
+if (src[a] == '\0')
+{
+break;
+}
+}
+while (b != n)
+dest[b++] = '\0';
+return (dest);
 }
