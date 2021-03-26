aa9989fa20299ebf08ccc26f9f3a34a54135adf7
root <vincentjayden49@gmail.com>
2021-03-26 16:34:54 +0000

    checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * get_endianness - determine endianness of system.
+ * Return: int, 0 if big endian. 1, if little.
+ */
+
+int get_endianness(void)
+{
+int n;
+
+n = 1;
+return ((int)((char *)&n)[0]);
+}
