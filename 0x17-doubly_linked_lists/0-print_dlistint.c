ee382fc2e516a9752002ece088dab3590f98fd71
root <vincentjayden49@gmail.com>
2021-05-03 14:04:10 +0000

    prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements.
+ * @h: pointer to the node.
+ *
+ * Return: address of the element or NULL if it failed
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count = 0;
+while (h)
+{
+if (h->n == 0)
+{
+printf("0 (nill)\n");
+}
+else
+{
+printf("%d \n", h->n);
+h = h->next;
+count++;
+}
+}
+return (count);
+}
