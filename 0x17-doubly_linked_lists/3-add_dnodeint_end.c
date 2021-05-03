e33808c03b8a46d2b02bef7ce8f6feaf9adba356
VinneyJ <vincentjayden49@gmail.com>
2021-05-03 23:16:25 +0300

    update add node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,23 +8,23 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-  dlistint_t *new, *tmp;
+dlistint_t *new, *tmp;
 
-  new = malloc(sizeof(dlistint_t));
-  if (!new)
-    {
-      printf("Error: malloc failed\n");
-      return (NULL);
-    }
-  new->n = n;
-  new->next = NULL;
-  new->prev = NULL;
-  if (!*head)
-    return (*head = new);
-  tmp = *head;
-  while (tmp->next)
-    tmp = tmp->next;
-  tmp->next = new;
-  new->prev = tmp;
-  return (new);
+new = malloc(sizeof(dlistint_t));
+if (!new)
+{
+printf("Error: malloc failed\n");
+return (NULL);
+}
+new->n = n;
+new->next = NULL;
+new->prev = NULL;
+if (!*head)
+return (*head = new);
+tmp = *head;
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+new->prev = tmp;
+return (new);
 }
